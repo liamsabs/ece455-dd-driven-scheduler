a0f10aff76d1373a0d6c7460d0d9d2f5e58d82da
Doctor-wonder <niallpeck@gmail.com>
2024-03-06 15:55:39 -0800

    made create_dd_task

--- src/dd_tasks.c
+++ src/dd_tasks.c
@@ -1,1 +1,29 @@
 #include "dd_tasks.h"
+
+void create_dd_task( TaskHandle_t t_handle,
+task_type type,
+uint32_t task_id,
+uint32_t absolute_deadline
+){
+    struct dd_task new_task;
+    new_task.t_handle = t_handle;
+    new_task.type = type;
+    new_task.task_id = task_id;
+    new_task.absolute_deadline = absolute_deadline;
+}
+
+void delete_dd_task(uint32_t task_id){
+
+}
+
+**dd_task_list get_active_dd_task_list(void){
+
+}
+
+**dd_task_list get_complete_dd_task_list(void){
+
+}
+
+**dd_task_list get_overdue_dd_task_list(void){
+
+}
