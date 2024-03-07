ae640fdce93e73b102ecdb70e6eb53f8ba460e03
Liam Sabourin <87916806+lsabs49@users.noreply.github.com>
2024-03-06 16:43:29 -0800

    fixed errors

--- src/dd_tasks.h
+++ src/dd_tasks.h
@@ -9,21 +9,20 @@
 #include "../FreeRTOS_Source/include/semphr.h"
 #include "../FreeRTOS_Source/include/task.h"
 #include "../FreeRTOS_Source/include/timers.h"
-
 #include "dd_task_list.h"
 
 // enum for state
 typedef enum {PERIODIC,APERIODIC} task_type;
 
 // deadline-driven schedule handler struct
-struct dd_task {
+typedef struct {
     TaskHandle_t t_handle;
     task_type type;
     uint32_t task_id;
     uint32_t release_time;
     uint32_t absolute_deadline;
     uint32_t completion_time;
-};
+} dd_task;
 
 
 void create_dd_task( TaskHandle_t t_handle,
@@ -34,8 +33,10 @@ uint32_t absolute_deadline
 
 void delete_dd_task(uint32_t task_id);
 
-**dd_task_list get_active_dd_task_list(void);
+dd_task_list** get_active_dd_task_list(void);
 
-**dd_task_list get_complete_dd_task_list(void);
+dd_task_list** get_complete_dd_task_list(void);
 
-**dd_task_list get_overdue_dd_task_list(void);
+dd_task_list** get_overdue_dd_task_list(void);
+
+#endif
